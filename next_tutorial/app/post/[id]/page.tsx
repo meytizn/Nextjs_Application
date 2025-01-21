ba0d22570cf2ebfb75c27c8974af34fd1cf1615b
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-21 21:05:05 +0330

    dynamic routing using param

---
+++ next_tutorial/app/post/[id]/page.tsx
@@ -0,0 +1,11 @@
+
+
+function PostId( {params} : {params : {id:string} } ){
+  return(<>
+  
+  <div>post id : {params.id}</div>
+
+  </>)
+}
+
+export default PostId
---
+++ next_tutorial/app/post/page.tsx
@@ -0,0 +1,10 @@
+
+function Post(){
+  return(
+    <>
+    <h3>post page usig param by id usig [] in create a new folder and tsxt file in that  . </h3>
+    </>
+  )
+}
+
+export default Post
