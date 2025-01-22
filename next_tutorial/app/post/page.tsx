fa29d09c939da56f322adc67a65f83099051c8c0
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-22 11:47:30 +0330

    cashing next revalidate

--- next_tutorial/app/post/page.tsx
+++ next_tutorial/app/post/page.tsx
@@ -11,7 +11,7 @@ interface PostInterface{
 
 async function Post(){
 
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts',{cache:'no-store',})
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts',{next:{revalidate:5},})
   let data:PostInterface[]=await response.json()
   data=data.slice(0,10) //to use 10 post
   return(
