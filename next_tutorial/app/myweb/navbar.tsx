9b872a60329db40fc0be613a4d8cd5e7825018fa
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-23 20:35:05 +0330

    navbar created successfully

--- next_tutorial/app/myweb/navbar.tsx
+++ next_tutorial/app/myweb/navbar.tsx
@@ -2,36 +2,57 @@
 import React, { useState } from "react";
 
 function Navbar() {
-  const [marginTop, setMarginTop] = useState("0px");
+  const [bottom, setbottom] = useState("-100px");
   const [open, setOpen] = useState(false);
 
   const handleClick = () => {
     setOpen(!open);
     if (!open) {
-      setMarginTop("100px");
+      setbottom("0px");
       console.log(open)
     } else {
-      setMarginTop("0px");
+      setbottom("-100px");
       console.log(open)
     }
   };
 
   return (
     <>
-      <div className="flex flex-col text-black bg-slate-100 md:flex-row md:bg-pink-400 text-center">
+
+
+{/* navbar */}
+
+<div style={{ bottom: `${bottom}` }} className={`  flex flex-col fixed -z-10 right-0 left-0 bottom-0  text-black bg-blue-800 md:flex-row md:bg-pink-400 text-center  transition-all delay-[5ms] md:top-0 md:h-[50px] my-0`}>
         <div id="firstdiv" className="w-[100%] md:w-[25%]">1</div>
         <div className="w-[100%] md:w-[25%]">2</div>
         <div className="w-[100%] md:w-[25%]">3</div>
         <div className="w-[100%] md:w-[25%]">4</div>
       </div>
 
-      <div
-        style={{ marginTop: `${marginTop}` }}
-        className="w-[20%] float-right text-center bg-slate-200 text-black my-[0px] transition-all delay-[5ms]"
+
+
+
+
+
+
+
+<div
+        // style={{ bottom: `${bottom}` }}
+        className="w-[20%] float-right text-center bg-blue-200 text-black my-[0px] transition-all delay-[5ms] fixed right-0 md:hidden "
         onClick={handleClick}
       >
         Tester
       </div>
+
+
+
+
+
+
+
+
+
+
     </>
   );
 }
