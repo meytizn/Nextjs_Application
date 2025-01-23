e06f493ae8058fc2faf8a6b22780ff5f8ad7a232
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-23 18:27:59 +0330

    navbar testing created

--- next_tutorial/app/myweb/navbar.tsx
+++ next_tutorial/app/myweb/navbar.tsx
@@ -1,29 +1,39 @@
 'use client'
+import React, { useState } from "react";
 
-import { useState } from "react"
+function Navbar() {
+  const [marginTop, setMarginTop] = useState("0px");
+  const [open, setOpen] = useState(false);
 
-function Navbar(){
+  const handleClick = () => {
+    setOpen(!open);
+    if (!open) {
+      setMarginTop("100px");
+      console.log(open)
+    } else {
+      setMarginTop("0px");
+      console.log(open)
+    }
+  };
 
-  let [age,Setage]=useState(0)
+  return (
+    <>
+      <div className="flex flex-col text-black bg-slate-100 md:flex-row md:bg-pink-400 text-center">
+        <div id="firstdiv" className="w-[100%] md:w-[25%]">1</div>
+        <div className="w-[100%] md:w-[25%]">2</div>
+        <div className="w-[100%] md:w-[25%]">3</div>
+        <div className="w-[100%] md:w-[25%]">4</div>
+      </div>
 
-const add=()=>{
-  const a = document.getElementById('firstdiv')
- 
-  return age+1
-
+      <div
+        style={{ marginTop: `${marginTop}` }}
+        className="w-[20%] float-right text-center bg-slate-200 text-black my-[0px] transition-all delay-[5ms]"
+        onClick={handleClick}
+      >
+        Tester
+      </div>
+    </>
+  );
 }
 
-
-  return(
-  <>
-  <div className="flex flex-col text-black  bg-slate-100  md:flex-row  md:bg-pink-400 text-center">
-      <div id="firstdiv" className="w-[100%] md:w-[25%] ">{age}</div>
-      <div className="w-[100%] md:w-[25%]">2</div>
-      <div className="w-[100%] md:w-[25%]">3</div>
-      <div onClick={()=>{Setage(add)}} className="w-[100%] md:w-[25%]">4</div>
-  </div>
-  </>
-  )
-}
-
-export default Navbar
+export default Navbar;
